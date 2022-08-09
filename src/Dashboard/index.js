f7dfce98b902a833f59a5f259c898e6138aa4eb2
Jonas Chow <jonaschow99@gmail.com>
2022-08-09 13:46:38 +0800

    setup webpack config

---
+++ src/Dashboard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.css";
+
+const Dashboard = () => {
+  return <div className={styles.header}>hello world!</div>
+};
+
+export default Dashboard;
